bfbb59759cb090ae6c0859ae849bef0b36781320
Hanamikid <75309108+Hanamikid@users.noreply.github.com>
2021-04-17 17:01:20 +0200

    Add files via upload

---
+++ Javascript_1_KovacsGyorgy/java.js
@@ -0,0 +1,22 @@
+function Alap() {
+    document.getElementById("eredmeny").innerHTML = "";
+    document.querySelector("#szurke-1-3").innerHTML = "";
+    document.querySelector("#piros-2-3").innerHTML = "";
+    document.querySelector("#szurke-3-3").innerHTML = "";
+    document.querySelector("#kek-2-2").innerHTML = "";
+    document.querySelector("#piros-3-2").innerHTML = "";
+    document.querySelector("#kek-1-1").innerHTML = "";
+    document.querySelector("#szurke-2-1").innerHTML = "";
+    document.querySelector("#piros-3-1").innerHTML = "";
+  }
+  function Megoldas() {
+      document.getElementById("eredmeny").innerHTML = "109";
+      document.querySelector("#szurke-1-3").innerHTML = "0";
+      document.querySelector("#piros-2-3").innerHTML = "9";
+      document.querySelector("#szurke-3-3").innerHTML = "0";
+      document.querySelector("#kek-2-2").innerHTML = "1";
+      document.querySelector("#piros-3-2").innerHTML = "9";
+      document.querySelector("#kek-1-1").innerHTML = "1";
+      document.querySelector("#szurke-2-1").innerHTML = "0";
+      document.querySelector("#piros-3-1").innerHTML = "9";
+    }
